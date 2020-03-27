09fede87f5a34d0c171f3bb33df5be357de725f7
Kip <lembuss@users.noreply.github.com>
2020-03-27 18:19:49 +0300

    Decided to use CDN instead of local bootstrap files

--- index.php
+++ index.php
@@ -3,12 +3,19 @@
 <head>
 	<title>Contact Us</title>
 	<meta charset="utf-8">
-	<script type="text/javascript" src="bootstrap/js/jQuery.js"></script>
-	<script type="text/javascript" src="bootstrap/js/popper.js"></script>
-	<script type="text/javascript" src="bootstrap/js/bootsrap.js"></script>
+	<!-- Latest compiled and minified CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
 
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="bootstrap/css/custom.css">
+	<!-- jQuery library -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+	<!-- Latest compiled JavaScript -->
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+
+	<!-- <link rel="stylesheet" type="text/css" href="custom.css"> -->
 </head>
 <body>
 <!-- jumbotron as page title	 -->
